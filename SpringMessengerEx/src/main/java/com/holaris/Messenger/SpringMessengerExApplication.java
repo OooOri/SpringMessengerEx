6526f6d6580989832ecf24d4761c907554e24470
DESKTOP-V5ECTB6\tjdrn <tjdrnr17@naver.com>
2018-09-29 20:53:33 +0900

    first commit setting

---
+++ SpringMessengerEx/src/main/java/com/holaris/Messenger/SpringMessengerExApplication.java
@@ -0,0 +1,12 @@
+package com.holaris.Messenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMessengerExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMessengerExApplication.class, args);
+	}
+}
---
+++ SpringMessengerEx/src/test/java/com/holaris/Messenger/SpringMessengerExApplicationTests.java
@@ -0,0 +1,16 @@
+package com.holaris.Messenger;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringMessengerExApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
